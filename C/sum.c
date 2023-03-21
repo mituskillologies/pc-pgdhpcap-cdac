d47c1741513005585e23db9a14bfc9fe72356f16
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2023-03-21 11:22:56 +0530

    Add files via upload

---
+++ C/sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	float num1, num2;
+	int float;
+	printf("Enter first number:");
+	scanf("%f", &num1);
+	printf("Enter second number:");
+	scanf("%f", &num2);
+	printf("The addition is %f\n",num1 + num2);
+	return 0;
+}
