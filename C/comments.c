4e0ff75ede16371292b5b8d8ce03b3e2585fdcb8
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2023-03-21 11:21:49 +0530

    Add files via upload

---
+++ C/comments.c
@@ -0,0 +1,10 @@
+// Author: Tushar B. Kute
+#include<stdio.h>
+int main() {
+	int num;
+	num = 5 + 6 * 2 - 15/3;
+	printf("Result: %i\n", num);
+	/* This will print the result
+	on screen */
+	return 0;
+}
