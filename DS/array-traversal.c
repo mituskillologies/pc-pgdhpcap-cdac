1171381176e525e16eee0a37e1619a2aeeb65568
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2023-03-29 10:05:13 +0530

    Add files via upload

---
+++ DS/array-traversal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+int main() {  
+	int Arr[5] = {18, 30, 15, 70, 12};  
+    int i;  
+    printf("Elements of the array are:\n");  
+    for(i = 0; i<5; i++) {  
+       printf("Arr[%d] = %d,  ", i, Arr[i]);  
+    }  
+    return 0;
+}  
